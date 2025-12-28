f4c47d4c01ad5ddf68743155118df7ebe78f3621
Kumar Effect <kumareffect@gmail.com>
2025-12-28 21:02:14 +0530

    done here

--- src/App.js
+++ src/App.js
@@ -1,47 +1,5 @@
-import { useState } from "react";
-
 const App = () => {
-  const [data, setData] = useState({
-    userName: "",
-    userPassword: "",
-  });
-
-  const onChange = (event) => {
-    setData((prev) => ({
-      ...prev,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
-  const validate = (event) => {
-    event.preventDefault();
-    console.log(data);
-  };
-
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <form onSubmit={validate}>
-        <input
-          type="text"
-          name="userName"
-          placeholder="Your username here"
-          value={data.userName}
-          onChange={onChange}
-        />
-        <br />
-        <input
-          type="password"
-          name="userPassword"
-          placeholder="Your password here"
-          value={data.userPassword}
-          onChange={onChange}
-        />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    </>
-  );
+  return <></>;
 };
 
 export default App;
