55046ddbc113c9d9a9a52f2d2430131da164a236
Kumar Effect <kumareffect@gmail.com>
2025-12-28 21:08:53 +0530

    useRef with focus background and color in input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { useRef } from "react";
+
 const App = () => {
-  return <></>;
+  const inputRef = useRef(null);
+
+  const changeMe = () => {
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = "red";
+    inputRef.current.style.color = "white";
+  };
+
+  return (
+    <>
+      <input type="text" placeholder="Your name" ref={inputRef} />
+
+      <input type="button" onClick={changeMe} value="Change me" />
+    </>
+  );
 };
 
 export default App;
